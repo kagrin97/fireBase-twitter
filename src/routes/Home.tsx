048c0e993f50c2092a0e253d8ab90cf7279be90c
MinGyu-Kang <kagrin97@gmail.com>
2022-09-29 12:09:54 +0900

    [#1] refactor : firebase에서 nweets를 가져오는 hook으로 리팩토링했습니다.
    기존에 home페이지에서 firebase에서 nweets를 가져오는 코드를 hook으로
    리팩토링해서 재사용성을 높였습니다.

--- src/routes/Home.tsx
+++ src/routes/Home.tsx
@@ -1,26 +1,10 @@
-import { dbService } from "../fbase";
-import { collection, query, onSnapshot, orderBy } from "firebase/firestore";
-import { useState, useEffect } from "react";
 import Nweet from "components/Nweet";
 import NweetFactory from "components/NweetFactory";
 
+import useGetNweets from "hooks/useGetNweets";
+
 const Home = ({ userObj }: { userObj: any }) => {
-  const [nweets, setNweets] = useState<string[]>([]);
-
-  useEffect(() => {
-    const q = query(
-      collection(dbService, "nweets"),
-      orderBy("createdAt", "desc")
-    );
-    onSnapshot(q, (snapshot) => {
-      const nweetArr: any = snapshot.docs.map((doc) => ({
-        id: doc.id,
-        ...doc.data(),
-      }));
-      setNweets(nweetArr);
-    });
-  }, []);
-
+  const { nweets } = useGetNweets();
   return (
     <div className={`w-full max-w-xl`}>
       <NweetFactory userObj={userObj} />
