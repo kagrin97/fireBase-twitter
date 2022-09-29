be0022e7e1f3ea1e59903088e40a1129c030325d
MinGyu-Kang <kagrin97@gmail.com>
2022-09-29 11:42:34 +0900

    [#1] refactor : 기존의 유저상태를 체크하는 함수들을 hook으로 따로 만들었습니다.
    hooks/useCheckUser.ts 파일안에 유저상태를 관리하는 함수로 hook을만들었습니다.

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,29 +1,8 @@
-import React, { useState, useEffect } from "react";
 import AppRouter from "components/Router";
-import { authService } from "../fbase";
-import { onAuthStateChanged, updateCurrentUser } from "firebase/auth";
+import useCheckUser from "hooks/useCheckUser";
 
 function App() {
-  const [init, setInit] = useState(false);
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  const [userObj, setUserObj] = useState<any>(null);
-  useEffect(() => {
-    onAuthStateChanged(authService, (user) => {
-      if (user) {
-        setIsLoggedIn(true);
-        setUserObj(user);
-      } else {
-        setIsLoggedIn(false);
-        setUserObj(null);
-      }
-      setInit(true);
-    });
-  }, []);
-
-  const refreshUser = async () => {
-    await updateCurrentUser(authService, authService.currentUser);
-    setUserObj(authService.currentUser);
-  };
+  const { init, isLoggedIn, userObj, refreshUser } = useCheckUser();
 
   return (
     <body
---
+++ src/hooks/useCheckUser.ts
@@ -0,0 +1,31 @@
+import { useEffect, useState } from "react";
+
+import { authService } from "../fbase";
+import { onAuthStateChanged, updateCurrentUser } from "firebase/auth";
+
+function useCheckUser() {
+  const [init, setInit] = useState(false);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userObj, setUserObj] = useState<any>(null);
+  useEffect(() => {
+    onAuthStateChanged(authService, (user) => {
+      if (user) {
+        setIsLoggedIn(true);
+        setUserObj(user);
+      } else {
+        setIsLoggedIn(false);
+        setUserObj(null);
+      }
+      setInit(true);
+    });
+  }, []);
+
+  const refreshUser = async () => {
+    await updateCurrentUser(authService, authService.currentUser);
+    setUserObj(authService.currentUser);
+  };
+
+  return { init, isLoggedIn, userObj, refreshUser };
+}
+
+export default useCheckUser;
