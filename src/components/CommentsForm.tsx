185554150df077bc719bb946b5c7d6f069de08e6
MinGyu-Kang <kagrin97@gmail.com>
2022-09-29 16:38:30 +0900

    [#1] refactor : NweetsFactory파일을 CommentsForm으로 바꾸고 비즈니스로직들을 hooks/useCommentsFrom으로 분리했습니다

--- src/components/CommentsForm.tsx
+++ src/components/CommentsForm.tsx
@@ -1,68 +1,25 @@
-import React, { useState, useRef } from "react";
-import { ref, uploadString, getDownloadURL } from "@firebase/storage";
-import { v4 as uuidv4 } from "uuid";
-import { dbService, storageService } from "../fbase";
-import { addDoc, collection } from "firebase/firestore";
+import useCommentsForm from "hooks/useCommentsForm";
 
 export default function CommentsForm({ userObj }: { userObj: any }) {
-  const [nweet, setNweet] = useState("");
-  const [attachment, setAttachment] = useState<any>("");
-  const fileInput: any = useRef();
-  const onSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    let attachmentUrl = "";
-    if (attachment !== "") {
-      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
-      const response = await uploadString(fileRef, attachment, "data_url");
-      attachmentUrl = await getDownloadURL(response.ref);
-    }
-    try {
-      const docRef = await addDoc(collection(dbService, "nweets"), {
-        text: nweet,
-        createdAt: Date.now(),
-        creatorId: userObj.uid,
-        nickname: userObj.displayName,
-        attachmentUrl,
-        photoUrl: userObj.photoURL,
-      });
-      console.info("Document written with ID: ", docRef.id);
-    } catch (error) {
-      console.error("Error adding document: ", error);
-    }
-    setNweet("");
-    setAttachment("");
-  };
+  const {
+    nweet,
+    attachment,
+    fileInput,
+    onSubmitComment,
+    onChangeText,
+    onPhotoChange,
+    onClearAttachment,
+  } = useCommentsForm(userObj);
 
-  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const {
-      target: { value },
-    } = event;
-    setNweet(value);
-  };
-
-  const onFileChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    const files: any = event.target.files;
-    const theFile = files[0];
-    const reader = new FileReader();
-    reader.onloadend = (finishedEvent: any) => {
-      const result = finishedEvent.currentTarget.result;
-      setAttachment(result);
-    };
-    reader.readAsDataURL(theFile);
-  };
-  const onClearAttachment = () => {
-    setAttachment(null);
-    fileInput.current.value = "";
-  };
   return (
     <form
-      onSubmit={onSubmit}
+      onSubmit={onSubmitComment}
       className={`flex flex-col mt-4 mx-auto text-center`}
     >
       <div>
         <input
           value={nweet}
-          onChange={onChange}
+          onChange={onChangeText}
           type="text"
           placeholder="채팅을 남겨보세요!"
           maxLength={90}
@@ -78,7 +35,7 @@ export default function CommentsForm({ userObj }: { userObj: any }) {
         <input
           type="file"
           accept="image/*"
-          onChange={onFileChange}
+          onChange={onPhotoChange}
           ref={fileInput}
           className={`file:mr-4 file:mt-4 file:py-2 file:px-4
         file:rounded-full file:border-0
---
+++ src/hooks/useCommentsForm.ts
@@ -0,0 +1,69 @@
+import React, { useState, useRef } from "react";
+import { ref, uploadString, getDownloadURL } from "@firebase/storage";
+import { v4 as uuidv4 } from "uuid";
+import { dbService, storageService } from "../fbase";
+import { addDoc, collection } from "firebase/firestore";
+
+function useCommentsForm(userObj: any) {
+  const [nweet, setNweet] = useState("");
+  const [attachment, setAttachment] = useState<any>("");
+  const fileInput: any = useRef();
+  const onSubmitComment = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    let attachmentUrl = "";
+    if (attachment !== "") {
+      const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
+      const response = await uploadString(fileRef, attachment, "data_url");
+      attachmentUrl = await getDownloadURL(response.ref);
+    }
+    try {
+      const docRef = await addDoc(collection(dbService, "nweets"), {
+        text: nweet,
+        createdAt: Date.now(),
+        creatorId: userObj.uid,
+        nickname: userObj.displayName,
+        attachmentUrl,
+        photoUrl: userObj.photoURL,
+      });
+      console.info("Document written with ID: ", docRef.id);
+    } catch (error) {
+      console.error("Error adding document: ", error);
+    }
+    setNweet("");
+    setAttachment("");
+  };
+
+  const onChangeText = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const {
+      target: { value },
+    } = event;
+    setNweet(value);
+  };
+
+  const onPhotoChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const files: any = event.target.files;
+    const theFile = files[0];
+    const reader = new FileReader();
+    reader.onloadend = (finishedEvent: any) => {
+      const result = finishedEvent.currentTarget.result;
+      setAttachment(result);
+    };
+    reader.readAsDataURL(theFile);
+  };
+  const onClearAttachment = () => {
+    setAttachment(null);
+    fileInput.current.value = "";
+  };
+
+  return {
+    nweet,
+    attachment,
+    onSubmitComment,
+    fileInput,
+    onChangeText,
+    onPhotoChange,
+    onClearAttachment,
+  };
+}
+
+export default useCommentsForm;
