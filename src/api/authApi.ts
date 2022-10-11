39c9f860b7567465bd8ac4b74f2f173bd603b3d4
MinGyu-Kang <kagrin97@gmail.com>
2022-10-11 13:14:51 +0900

    refactor : 사용자 프로필 변경시 채팅의 유저 정보를 모두 변경하는 기능을 authApi -> docsApi로 분리했습니다.

--- src/api/authApi.ts
+++ src/api/authApi.ts
@@ -1,8 +1,9 @@
-import { dbService, storageService } from "../fbase";
-import { collection, getDocs, query, updateDoc, doc } from "firebase/firestore";
+import { storageService } from "../fbase";
 import { updateProfile } from "firebase/auth";
 import { ref, uploadString, getDownloadURL } from "@firebase/storage";
 import { v4 as uuidv4 } from "uuid";
+
+import DocsApi from "api/docsApi";
 
 interface AuthType {
   userObj: any;
@@ -12,33 +13,18 @@ interface AuthType {
 }
 
 class AuthApi {
-  static async updateDocs({ userObj, newDisplayName, photoUrl }: AuthType) {
-    const q = query(collection(dbService, "nweets"));
-    const querySnapshot = await getDocs(q);
-    querySnapshot.forEach(async (document) => {
-      if (document.data().creatorId === userObj.uid) {
-        const NweetTextRef = doc(dbService, "nweets", `${document.id}`);
-        await updateDoc(NweetTextRef, {
-          nickname: newDisplayName,
-          photoUrl,
-        });
-      }
-    });
-  }
-
   static async changeProfile({ userObj, newDisplayName, photo }: AuthType) {
-    if (userObj.displayName !== newDisplayName || photo !== "") {
+    if (userObj.displayName !== newDisplayName || userObj.photoURL !== photo) {
       if (userObj.displayName !== newDisplayName) {
         await updateProfile(userObj, { displayName: newDisplayName });
       }
-      let photoUrl = "";
-      if (photo !== "") {
+      if (userObj.photoURL !== photo) {
         const fileRef = ref(storageService, `${userObj.uid}/${uuidv4()}`);
         const response = await uploadString(fileRef, photo!, "data_url");
-        photoUrl = await getDownloadURL(response.ref);
-        await updateProfile(userObj, { photoURL: photoUrl });
+        photo = await getDownloadURL(response.ref);
+        await updateProfile(userObj, { photoURL: photo });
       }
-      AuthApi.updateDocs({ userObj, newDisplayName, photoUrl });
+      DocsApi.updateDocs({ userObj, newDisplayName, photoUrl: photo });
     }
   }
 }
