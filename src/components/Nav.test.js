867e573bfb73925ae9ad969789e99c217183c164
MinGyu-Kang <kagrin97@gmail.com>
2022-10-12 14:45:27 +0900

    [#6] feat : Nav컴포넌트에 Link태그에 href값이 제대로 들어있는지 테스트코드 작성

--- src/components/Nav.test.js
+++ src/components/Nav.test.js
@@ -6,11 +6,24 @@ import Nav from "./Nav";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
-test("3개의 Link 태그에 hover하면 color가 yellow-500으로 바뀌는지 확인", async () => {
-  render(<Nav />, { wrapper: BrowserRouter });
-  const LinkBtn3 = screen.getAllByRole("link");
-  LinkBtn3.forEach((el) => {
-    fireEvent.mouseOver(el);
-    expect(el).toHaveStyle({ color: "yellow-500" });
+describe("Link 태그 검사", () => {
+  test("3개의 Link 태그에 hover하면 color가 yellow-500으로 바뀌는지 확인", () => {
+    render(<Nav />, { wrapper: BrowserRouter });
+    const link3 = screen.getAllByRole("link");
+    link3.forEach((el) => {
+      userEvent.hover(el);
+      expect(el).toHaveStyle({ color: "yellow-500" });
+    });
+  });
+
+  test("3개의 Link 태그에 href값이 존재하는지", () => {
+    render(<Nav />, { wrapper: BrowserRouter });
+    const homeLink = screen.getByText("Home");
+    const profileLink = screen.getByText("Profile");
+    const logOutLink = screen.getByText("Home");
+
+    expect(homeLink).toHaveAttribute("href", "/");
+    expect(profileLink).toHaveAttribute("href", "/profile");
+    expect(logOutLink).toHaveAttribute("href", "/");
   });
 });
