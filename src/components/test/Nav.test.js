11a93ca3671cd3475cfa042ca3f1f36abf45d6a0
MinGyu-Kang <kagrin97@gmail.com>
2022-10-12 16:27:45 +0900

    [#6] refactor : Nav.test.js 파일을 components/test 폴더로 옮겼습니다.

--- src/components/test/Nav.test.js
+++ src/components/test/Nav.test.js
@@ -2,7 +2,7 @@
 import React from "react";
 import { render, screen, fireEvent, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import Nav from "./Nav";
+import Nav from "../Nav";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
