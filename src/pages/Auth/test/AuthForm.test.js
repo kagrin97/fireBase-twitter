babec5d9fc81adfd9da06121b116b6b5a7029da7
MinGyu-Kang <kagrin97@gmail.com>
2022-10-13 14:54:44 +0900

    [#6] feat : AuthForm.test.js에 이메일, 비밀번호 input이 잘 작동하는지 test code 작성

--- src/pages/Auth/test/AuthForm.test.js
+++ src/pages/Auth/test/AuthForm.test.js
@@ -5,10 +5,15 @@ import AuthForm from "../components/AuthForm";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
+let setStateMock = jest.fn();
+const useStateMock = (useState) => [useState, setStateMock];
+jest.spyOn(React, "useState").mockImplementation(useStateMock);
+
 describe("회원가입, 로그인 버튼 작동하는지", () => {
-  const setStateMock = jest.fn();
-  const useStateMock = (useState) => [useState, setStateMock];
-  jest.spyOn(React, "useState").mockImplementation(useStateMock);
+  beforeEach(() => {
+    setStateMock.mockClear();
+    setStateMock = jest.fn();
+  });
 
   test("회원가입 버튼 작동하는지", () => {
     render(<AuthForm />, { wrapper: BrowserRouter });
@@ -24,5 +29,34 @@ describe("회원가입, 로그인 버튼 작동하는지", () => {
     userEvent.click(signInButton);
 
     expect(setStateMock).toHaveBeenCalledWith(false);
+  });
+});
+
+describe("이메일, 비밀번호 input이 잘 작동하는지", () => {
+  beforeEach(() => {
+    setStateMock.mockClear();
+    setStateMock = jest.fn();
+  });
+
+  test("이메일 input이 잘 작동하는지", () => {
+    render(<AuthForm />, { wrapper: BrowserRouter });
+    const emailInput = screen.getByPlaceholderText(/Email/i);
+    const email = "kagrin97@gmail.com".split("");
+    userEvent.type(emailInput, "kagrin97@gmail.com");
+
+    email.forEach((val, index) =>
+      expect(setStateMock).toHaveBeenNthCalledWith(index + 1, val)
+    );
+  });
+
+  test("비밀번호 input이 잘 작동하는지", () => {
+    render(<AuthForm />, { wrapper: BrowserRouter });
+    const passwordInput = screen.getByPlaceholderText(/Password/i);
+    const password = "12345678".split("");
+    userEvent.type(passwordInput, "12345678");
+
+    password.forEach((val, index) =>
+      expect(setStateMock).toHaveBeenNthCalledWith(index + 1, val)
+    );
   });
 });
