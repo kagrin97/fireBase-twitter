4ab2eb8c2b6929a897c347e8cc4a8db3e0df92cb
MinGyu-Kang <kagrin97@gmail.com>
2022-10-13 14:23:41 +0900

    [#6] feat : Profile.test.js에 display name이 변경, 아바타 사진이 변경버튼이 존재 test code 작성

--- src/pages/Profile/test/Profile.test.js
+++ src/pages/Profile/test/Profile.test.js
@@ -5,10 +5,26 @@ import Profile from "../Profile";
 import { BrowserRouter, MemoryRouter } from "react-router-dom";
 import userEvent from "@testing-library/user-event";
 
-describe("<Profile /> 컴포넌트의 useState 작동하는지", () => {
+const event = { target: { files: { 0: { name: "아바타 사진.png" } } } };
+
+describe("<Profile /> 컴포넌트 작동하는지", () => {
   const setStateMock = jest.fn();
   const useStateMock = (useState) => [useState, setStateMock];
   jest.spyOn(React, "useState").mockImplementation(useStateMock);
+
+  test("display name이 변경되는지", () => {
+    render(<Profile />, { wrapper: BrowserRouter });
+    const inputDisplayName = screen.getByPlaceholderText(/Display name/i);
+    userEvent.type(inputDisplayName, "강민규");
+
+    expect(inputDisplayName).toHaveValue("강민규");
+  });
+
+  test("아바타 사진이 변경버튼이 존재하는지", () => {
+    render(<Profile />, { wrapper: BrowserRouter });
+    const changePhotoButton = screen.getByText("+");
+    expect(changePhotoButton).toBeInTheDocument();
+  });
 
   test("onClearPhoto 함수가 작동하는지", () => {
     render(<Profile />, { wrapper: BrowserRouter });
